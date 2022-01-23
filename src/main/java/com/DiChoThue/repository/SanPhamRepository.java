48aa00d5cfcbcb2bb97b447226e918a79648b603
huynhthinhi <huynhthinhi206@gmail.com>
2022-01-23 15:06:48 +0700

    create features for sanpham

---
+++ src/main/java/com/DiChoThue/repository/SanPhamRepository.java
@@ -0,0 +1,10 @@
+package com.DiChoThue.repository;
+
+import com.DiChoThue.model.SanPham;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SanPhamRepository extends JpaRepository<SanPham, Integer> {
+
+}
